740d67fed6693e7312a16d8f295ef0dad2a8b559
Brian Kim <kim.brian94@gmail.com>
2020-06-10 21:45:21 -0400

    Initial Commit

---
+++ To Do List/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Brian Kim on 2020-06-10.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ To Do List/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Brian Kim on 2020-06-10.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
