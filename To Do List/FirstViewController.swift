837b115e4bec6e1cf7cc8956ddab014f60119fc1
Brian Kim <kim.brian94@gmail.com>
2020-09-08 10:37:44 -0400

    First

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var table: UITableView!
+    var array: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            array.remove(at: indexPath.row)
+            UserDefaults.standard.set(array, forKey: "array")
+            table.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        let arrayObject = UserDefaults.standard.object(forKey: "array")
+        if let tempArray = arrayObject as? [String] {
+            array = tempArray
+        }
+        table.reloadData()
+    }
 }
 
